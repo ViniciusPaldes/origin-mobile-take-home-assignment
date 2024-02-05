c7d4664b77660a1630eb16b9e1efa1b260e1d441
ViniciusPaldes <vinnydp@gmail.com>
2024-02-05 08:54:53 -0300

    Fix Transaction prop on Transaction Item List

--- src/components/transaction-item/index.tsx
+++ src/components/transaction-item/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {TransactionVM} from '../../viewModel/Transaction';
 import {styles} from './style';
+import {Transaction} from '../../model/transaction';
 
 type TransactionItemProps = {
-  transaction: TransactionVM;
+  transaction: Transaction;
 };
 
 const TransactionItem: React.FC<TransactionItemProps> = ({transaction}) => {
