7be9540b20a5668d61ccc79804e1b9326181fad9
ViniciusPaldes <vinnydp@gmail.com>
2024-02-06 15:05:53 -0300

    Modern UI for login screen

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
-import {ActivityIndicator, Alert, Platform} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, Alert, Keyboard, Platform} from 'react-native';
 import {signIn} from '../../services/auth';
 import {
+  InputsContainer,
+  LogoContainer,
   StyledButton,
   StyledButtonText,
   StyledForgotPasswordButton,
@@ -18,6 +20,23 @@ const LoginScreen = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState<boolean>(false);
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => setKeyboardVisible(true),
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => setKeyboardVisible(false),
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
 
   const handleLogin = () => {
     setLoading(true);
@@ -44,43 +63,45 @@ const LoginScreen = ({navigation}) => {
 
   return (
     <StyledKeyboardAvoidingView
+      keyboardOpen={keyboardVisible}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}>
-      <StyledLogo
-        source={require('../../../assets/logo.png')}
-        resizeMode="contain"
-      />
-      <StyledTitle>Welcome to Transactions App</StyledTitle>
-      <StyledInput
-        onChangeText={text => setEmail(text)}
-        value={email}
-        placeholder="Email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <StyledInput
-        onChangeText={text => setPassword(text)}
-        value={password}
-        placeholder="Password"
-        secureTextEntry
-        autoCapitalize="none"
-      />
-      <StyledButton onPress={handleLogin}>
-        {loading ? (
-          <ActivityIndicator size="small" />
-        ) : (
-          <StyledButtonText>Sign In</StyledButtonText>
-        )}
-      </StyledButton>
-      <StyledSignUpButton onPress={() => navigation.navigate('SignUp')}>
-        <StyledSignUpButtonText>Sign Up</StyledSignUpButtonText>
-      </StyledSignUpButton>
-      <StyledForgotPasswordButton
-        onPress={() => navigation.navigate('ForgotPassword')}>
-        <StyledForgotPasswordButtonText>
-          Forgot Password?
-        </StyledForgotPasswordButtonText>
-      </StyledForgotPasswordButton>
+      <LogoContainer>
+        <StyledLogo source={require('../../../assets/logo.png')} />
+      </LogoContainer>
+      <InputsContainer keyboardOpen={keyboardVisible}>
+        <StyledTitle>Welcome to Transactions App</StyledTitle>
+        <StyledInput
+          onChangeText={text => setEmail(text)}
+          value={email}
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <StyledInput
+          onChangeText={text => setPassword(text)}
+          value={password}
+          placeholder="Password"
+          secureTextEntry
+          autoCapitalize="none"
+        />
+        <StyledButton onPress={handleLogin}>
+          {loading ? (
+            <ActivityIndicator size="small" />
+          ) : (
+            <StyledButtonText>Sign In</StyledButtonText>
+          )}
+        </StyledButton>
+        <StyledSignUpButton onPress={() => navigation.navigate('SignUp')}>
+          <StyledSignUpButtonText>Sign Up</StyledSignUpButtonText>
+        </StyledSignUpButton>
+        <StyledForgotPasswordButton
+          onPress={() => navigation.navigate('ForgotPassword')}>
+          <StyledForgotPasswordButtonText>
+            Forgot Password?
+          </StyledForgotPasswordButtonText>
+        </StyledForgotPasswordButton>
+      </InputsContainer>
     </StyledKeyboardAvoidingView>
   );
 };
--- src/screens/login/style.ts
+++ src/screens/login/style.ts
@@ -1,17 +1,44 @@
+import {Dimensions, ViewProps} from 'react-native';
 import styled from 'styled-components/native';
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
 
 export const StyledKeyboardAvoidingView = styled.KeyboardAvoidingView`
   flex: 1;
-  justify-content: center;
   align-items: center;
-  padding: 20px;
-  background-color: #fafbf6;
+  background-color: '#f0f0f0';
+`;
+
+export const LogoContainer = styled.View`
+  position: absolute;
+  top: 0;
+  height: ${screenHeight * 0.35}px;
+  width: ${screenWidth}px;
+  overflow: hidden;
 `;
 
 export const StyledLogo = styled.Image`
-  width: 150px;
-  height: 150px;
-  margin-bottom: 20px;
+  width: 100%;
+  height: 100%;
+`;
+
+interface InputContainerProps extends ViewProps {
+  keyboardOpen: boolean;
+}
+
+export const InputsContainer = styled.View<InputContainerProps>`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #f0f0f0;
+  border-top-right-radius: 25px;
+  border-top-left-radius: 25px;
+  padding: 20px;
+  margin-top: ${({keyboardOpen}) => {
+    return keyboardOpen ? '24px' : `${screenHeight * 0.3}px`;
+  }};
 `;
 
 export const StyledTitle = styled.Text`
